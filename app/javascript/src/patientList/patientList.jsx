55d0ba278b0211c7555130a05362d7c4b7d3dc3c
Mario <mario.perez094@gmail.com>
2023-12-06 11:25:01 -0700

    organizer

--- app/javascript/src/patientList/patientList.jsx
+++ app/javascript/src/patientList/patientList.jsx
@@ -2,9 +2,51 @@ import React from "react";
 
 import Navbar from '../components/navbar'
 import PatientWidget from "./patientWidget";
+import { sort } from "webpack/lib/dependencies/DependencyReference";
 
 class PatientList extends React.Component {
+  state = {
+    patients: [{
+      id: 1,
+      fName: 'John',
+      lName: 'Feta',
+      gender: 'Male',
+      age: 62,
+      diagnosis: 'Shortness of breath',
+      admission: '12/06/2023',
+    },
+    {
+      id: 3,
+      fName: 'Sam',
+      lName: 'Goota',
+      gender: 'Male',
+      age: 62,
+      diagnosis: 'Shortness of breath',
+      admission: '12/06/2023',
+    },
+    {
+      id: 2,
+      fName: 'Jane',
+      lName: 'Cheddar',
+      gender: 'Female',
+      age: 62,
+      diagnosis: 'Congestive Heart Failure',
+      admission: '12/02/2023',
+    }],
+    category: 'id',
+  }
+
+  sortByID() {
+    this.setState({ patients: this.state.patients.sort((a, b) => {return a.id - b.id}), category: 'id'})
+  }
+
+  sortByLastName() {
+    console.log(this.state.patients.sort((a, b) => { return a.lName < b.lName ? -1 : 1 }))
+    this.setState({ patients: this.state.patients.sort((a, b) => { return a.lName < b.lName ? -1 : 1 }), category: 'lName'})
+  }
+
   render() {
+    const {patients, category} = this.state;
     return (
       <>
         <Navbar />
@@ -16,44 +58,21 @@ class PatientList extends React.Component {
                 <table className="table table-responsive">
                   <thead className="list-header">
                     <tr>
-                      <th scope="col">ID</th>
-                      <th scope="col">Patient</th>
+                      <th scope="col"><a onClick={() => this.sortByID()}>ID {category === 'id' ? 'v' : '>'}</a></th>
+                      <th scope="col"><a onClick={() => this.sortByLastName()}>Patient {category === 'lName' ? 'v' : '>'}</a></th>
                       <th className="d-none d-md-table-cell" scope="col">Diagnosis</th>
                       <th className="d-none d-md-table-cell" scope="col">Length of Stay</th>
                       <th className="d-md-none">LOS</th>
                     </tr>
                   </thead>
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
-                  <PatientWidget />
+                  <tbody>
+                    {patients.length > 0
+                      ? patients.map((patient) => {
+                        return <PatientWidget key={patient.id} patient={patient} />
+                      })
+                      : <p>No patients found.</p> 
+                    }
+                  </tbody>
                 </table>
               </div>
             </div>
--- app/javascript/src/patientList/patientWidget.jsx
+++ app/javascript/src/patientList/patientWidget.jsx
@@ -2,39 +2,26 @@ import React from 'react';
 
 import '../patientList/patientList.scss'
 
-class PatientWidget extends React.Component {
-  state = {
-    patient: {
-      id: 1,
-      name: 'John Doe',
-      gender: 'Male',
-      age: 62,
-      diagnosis: 'Shortness of breath',
-      admission: '12/06/2023',
-    }
-  }
-  render() {
-    const { patient } = this.state;
-    const { id, name, gender, age, diagnosis, admission } = patient;
+const PatientWidget = (props) => {
 
-    return (
-      <tbody>
-        <tr>
-          <th scope='row'>{id}</th>
-          <td className='row'>
-            <div className='col-12'>
-              {name}
-            </div>
-            <div className='col-12'>
-              {age} | {gender}
-            </div>
-          </td>
-          <td className='d-none d-md-table-cell'>{diagnosis}</td>
-          <td>{admission}</td>
-        </tr>
-      </tbody>
-    )
-  }
+  const { patient } = props;
+  const { id, fName, lName, gender, age, diagnosis, admission } = patient;
+
+  return (
+    <tr>
+      <th scope='row'>{id}</th>
+      <td className='row'>
+        <div className='col-12'>
+          {lName}, {fName}
+        </div>
+        <div className='col-12'>
+          {age} | {gender}
+        </div>
+      </td>
+      <td className='d-none d-md-table-cell'>{diagnosis}</td>
+      <td>{admission}</td>
+    </tr>
+  )
 }
 
 export default PatientWidget;
